d26902318e6f43ba8ac7feea1dd6e4325ec3074e
Maximilian Kaske <maximilian@kaske.org>
2022-11-24 17:03:02 +0100

    wip

---
+++ README.md
@@ -0,0 +1,6 @@
+### CLI testing purpose
+
+Shortcuts:
+
+- [npm](https://www.npmjs.com/package/@mxkaske/deno-hello-world)
+- [deno](https://deno.land/x/deno_hello_world@v0.0.1)
--- cmd/build.ts
+++ cmd/build.ts
@@ -1,10 +1,13 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.23.0/mod.ts";
+import { copy } from "https://deno.land/std@0.165.0/fs/mod.ts";
 
 const packageManager = "npm";
 const outDir = "./dist";
 const version = Deno.args[0] ?? "development";
 
 await emptyDir(outDir);
+// REMINDER: copy content into dist
+await copy("content", "dist/content");
 
 await build({
   packageManager,
