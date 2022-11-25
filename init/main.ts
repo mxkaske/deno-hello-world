d3e38dd727e726e28dd13f63e597c7ba2bdbccb0
Maximilian Kaske <maximilian@kaske.org>
2022-11-25 20:38:19 +0100

    test: wrtie file

--- init/main.ts
+++ init/main.ts
@@ -1,7 +1,13 @@
 import * as path from "https://deno.land/std@0.165.0/path/mod.ts";
 import * as fs from "https://deno.land/std@0.165.0/fs/mod.ts";
 
-const resources = [{ path: "content/test.json" }, { path: "content/test.ts" }];
+const resources = [
+  {
+    read: "../content/test.json",
+    write: "/test.json",
+  },
+  { read: "../content/test.ts", write: "/test.ts" },
+];
 
 export async function initProject(folder: string, name: string) {
   const root = path.join(folder, name);
@@ -12,10 +18,11 @@ export async function initProject(folder: string, name: string) {
   console.log("generating...");
   await fs.ensureDir(root);
   for (const resource of resources) {
-    const fp = path.join(root, resource.path);
-    await fs.ensureFile(fp);
-    const bytes = await Deno.readFile(fp);
-    await Deno.writeFile(fp, bytes);
+    const fp_read = path.join(root, resource.read);
+    const fp_write = path.join(root, resource.write);
+    await fs.ensureFile(fp_read);
+    const bytes = await Deno.readFile(fp_read);
+    await Deno.writeFile(fp_write, bytes);
   }
   console.log("done.");
 }
