1ca515f619dedc48dddeed472806f1f31a9ee7f2
Maximilian Kaske <maximilian@kaske.org>
2022-11-25 13:42:16 +0100

    test: new file path

--- mod.ts
+++ mod.ts
@@ -2,7 +2,7 @@ import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
 import { copySync } from "https://deno.land/std@0.165.0/fs/mod.ts";
 import * as path from "https://deno.land/std@0.165.0/path/mod.ts";
 
-const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+const filePath = path.fromFileUrl(new URL("content", import.meta.url));
 
 await new Command()
   // Main command.
@@ -12,7 +12,7 @@ await new Command()
   .option("-d, --dir", "Name of the directory")
   .arguments("<directory>")
   .action((options, ...args) => {
-    copySync(`${__dirname}/content`, args[0]);
+    copySync(filePath, args[0]);
     // Deno.mkdir(args[0]);
     console.log(`Next steps:\ncd ${args[0]}\nnpm i\nnpm run dev`);
   })
